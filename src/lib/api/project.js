0640ee738abb0049cdcc9d20c3ce2f1b77ece8e9
Injeong Kim <ally.duckling@gmail.com>
2019-09-20 17:55:42 +0900

    - token 저장소를 cookie로 변경 - 로그인 하지 않았을 때 / 로그아웃 후  company page로 전환되도록 수정

--- src/lib/api/project.js
+++ src/lib/api/project.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const AUTH_TOKEN = localStorage.getItem('token') || sessionStorage.getItem('token');
+const hasTokenCookie = document.cookie.split(';').map(c => c).find(x => x.indexOf('token=') > 0);
+console.log(hasTokenCookie);
+const AUTH_TOKEN = hasTokenCookie !== undefined ? hasTokenCookie.replace(/\s/gi, '').substring(6) : null;
 const baseURL = 'https://qa-server.realdopt.com/api';
+// const baseURL = '';
 const headers = {
   headers: {
     'Content-Type': 'application/json',

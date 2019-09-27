05664e611dc54cfdece50f8921726e2d5782d877
Injeong Kim <ally.duckling@gmail.com>
2019-09-28 00:51:38 +0900

    extra info input add

--- src/containers/NewTestForm/TesrForm/TestFormTarget.js
+++ src/containers/NewTestForm/TesrForm/TestFormTarget.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormInput from 'components/FormInput';
 import FormSelect from 'components/FormSelect';
 import { Field } from 'redux-form';
@@ -9,10 +9,33 @@ const maxAgeVerify = value => (value && parseInt(value, 10) < 101 ? undefined : 
 const genderRequired = value => (value ? undefined : '성별을 선택해 주세요');
 
 const TestFormTarget = (props) => {
+  const [extraInfoBox, setInfoBox] = useState([0]);
   const { extraInfoCategory, isDisabled } = props;
   const genderCategory = [
     '남자', '여자', '무관',
   ];
+  console.log(extraInfoBox);
+
+  const addInfoBox = (num) => {
+    if (extraInfoBox.length > 2) {
+      alert('추가 정보는 3개까지 추가 가능합니다');
+      return false;
+    }
+    return setInfoBox([
+      ...extraInfoBox,
+      num,
+    ]);
+  };
+
+  const removeInfoBox = () => {
+    const tempArr = extraInfoBox.slice();
+    const resultArr = tempArr.slice(0, -1);
+
+    if (extraInfoBox.length < 2) {
+      return false;
+    }
+    return setInfoBox(resultArr);
+  };
 
   return (
     <div className="field-wrapper--target">
@@ -20,6 +43,7 @@ const TestFormTarget = (props) => {
         <div className="field">
           <span className="field__title">
             <strong className="title">누가 이 서비스를 주로 이용하나요? (타겟 정보)*</strong>
+            <span className="subtitle">타겟 인원수는 15명 입니다</span>
           </span>
           <p className="box-field">
             <Field
@@ -63,26 +87,34 @@ const TestFormTarget = (props) => {
           <span className="field__title">
             <strong className="title">추가 정보 선택</strong>
           </span>
-          <Field
-            name="extraInfoCategory"
-            type="select"
-            defaultValue="추가 정보 선택"
-            component={FormSelect}
-            disabled={isDisabled}
-          >
-            <option value="추가 정보 선택" disabled>추가 정보 선택</option>
-            {extraInfoCategory.map(m => (
-              <option key={m} value={m}>{m}</option>
-            ))}
-          </Field>
-          <Field
-            name="extraInfoDesc"
-            type="text"
-            label="target.extraInfoDesc"
-            placeholder="텍스트 입력"
-            component={FormInput}
-            disabled={isDisabled}
-          />
+          {extraInfoBox.map(n => (
+            <div className="field__box" key={n}>
+              <Field
+                name="extraInfoCategory"
+                type="select"
+                defaultValue="추가 정보 선택"
+                component={FormSelect}
+                disabled={isDisabled}
+              >
+                <option value="추가 정보 선택" disabled>추가 정보 선택</option>
+                {extraInfoCategory.map(m => (
+                  <option key={m} value={m}>{m}</option>
+                ))}
+              </Field>
+              <Field
+                name="extraInfoDesc"
+                type="text"
+                label="target.extraInfoDesc"
+                placeholder="텍스트 입력"
+                component={FormInput}
+                disabled={isDisabled}
+              />
+              {n === 0
+                ? <button type="button" className="btn-target-add" onClick={() => addInfoBox(extraInfoBox.length)}>타겟 추가하기</button>
+                : <button type="button" className="btn-target-remove" onClick={() => removeInfoBox()}>타겟 제거하기</button>
+              }
+            </div>
+          ))}
         </div>
         <div className="field">
           <span className="field__title">

bff370d45d7a3c96a21911ffd6b491f30d9dcff2
Injeong Kim <ally.duckling@gmail.com>
2019-10-01 21:08:40 +0900

    Payment tab in my page completed

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -24,8 +24,9 @@ const phoneNumRegexp = value => (value && !/^(01[016789]{1}|02|0[3-9]{1}[0-9]{1}
 
 class MyPage extends Component {
   state = {
-    onEdit: false,
     isLoading: false,
+    isProfileTab: true,
+    onEdit: false,
     selectedFile: null,
   }
 
@@ -65,16 +66,16 @@ class MyPage extends Component {
     });
   }
 
+  handleTabToggle = () => {
+    this.setState(prevState => ({
+      isProfileTab: !prevState.isProfileTab,
+    }));
+  }
+
   handleEdit = () => {
     this.setState(prevState => ({
       onEdit: !prevState.onEdit,
     }));
-  }
-
-  onReset = () => {
-    const { reset, onPopup } = this.props;
-    reset();
-    onPopup(false);
   }
 
   handlePwPopup = (e) => {
@@ -82,6 +83,12 @@ class MyPage extends Component {
 
     e.preventDefault();
     props.togglePopup(true);
+  }
+
+  onReset = () => {
+    const { reset, onPopup } = this.props;
+    reset();
+    onPopup(false);
   }
 
   onSubmit = (values) => {
@@ -118,9 +125,10 @@ class MyPage extends Component {
     } = this.props;
     const name = fieldValues !== undefined ? fieldValues.name : undefined;
     const phone = fieldValues !== undefined ? fieldValues.phone : undefined;
-    const { onEdit, isLoading } = this.state;
+    const { isProfileTab, isLoading, onEdit } = this.state;
     const {
       handleFileInput,
+      handleTabToggle,
       handleEdit,
       handlePwPopup,
       onSubmit,
@@ -168,120 +176,170 @@ class MyPage extends Component {
             <section className="contents__account">
               <div className="contents-inner">
                 <ul className="account__tablist">
-                  <li className="tablist__item--active"><button type="button" className="btn-tab">Profile</button></li>
-                  <li className="tablist__item"><button type="button" className="btn-tab">Payment</button></li>
+                  <li className={`tablist__item${isProfileTab ? '--active' : ''}`}>
+                    <button type="button" className="btn-tab" onClick={() => handleTabToggle()}>Profile</button>
+                  </li>
+                  <li className={`tablist__item${isProfileTab ? '' : '--active'}`}>
+                    <button type="button" className="btn-tab" onClick={() => handleTabToggle()}>Payment</button>
+                  </li>
                 </ul>
-                <div className="account__info">
-                  <section className="info__profile">
-                    <h2 className="info__title--hidden">Account</h2>
-                    <form className="profile__image" encType="multipart/form-data">
-                      <span className="box-image">
-                        <img src={avatar_url} alt="프로필" />
-                      </span>
-                      <span className="box-input">
-                        <label htmlFor="profile">
-                          <input type="file" name="profile" onChange={e => handleFileInput(e)} />
-                        </label>
-                      </span>
-                    </form>
-                    <span className="profile__desc">
-                      <strong className="desc__name">
-                        {prevName !== undefined && prevName !== ''
-                          ? prevName
-                          : email.substring(0, email.indexOf('@'))
-                        }
-                      </strong>
-                      <button type="button" className="btn-edit" onClick={() => handleEdit()}>Edit profile</button>
-                      <span className="desc__mail">{email}</span>
-                    </span>
-                  </section>
-                  {onEdit
-                    ? (
-                      <form className="form-account" onSubmit={handleSubmit(values => onSubmit(values))}>
-                        <section className="form__section">
-                          <p className="form__data-wrapper">
-                            <span className="wrapper__title">
-                              <strong className="title">이름*</strong>
-                            </span>
-                            <Field
-                              name="name"
-                              type="text"
-                              label="name"
-                              placeholder="텍스트 입력"
-                              component={FormInput}
-                              validate={[nameRequired, nameLength]}
-                            />
-                          </p>
-                          <p className="form__data-wrapper">
-                            <span className="wrapper__title">
-                              <strong className="title">연락처*</strong>
-                            </span>
-                            <Field
-                              name="phone"
-                              type="tel"
-                              label="phone"
-                              placeholder="‘-’ 제외하고 입력"
-                              component={FormInput}
-                              validate={[phoneNumRequired, phoneNumRegexp]}
-                            />
-                          </p>
-                        </section>
-                        <section className="form__section">
-                          <p className="form__data-wrapper">
-                            <span className="wrapper__title">
-                              <strong className="title">비밀번호</strong>
-                            </span>
-                            <button type="button" className="btn-changePw" onClick={e => handlePwPopup(e)}>비밀번호 수정</button>
-                          </p>
-                        </section>
-                        <div className="form__btn-wrapper">
-                          <button type="button" className="btn-cancle" onClick={onReset}>취소</button>
-                          <button type="submit" className={`btn-submit${name !== undefined && name !== '' && phone !== undefined && phone !== '' ? '--active' : ''}`} onClick={() => handleEdit}>확인</button>
-                        </div>
-                      </form>
-                    )
-                    : (
-                      <>
-                        <section className="info__teams">
-                          <h2 className="info__title">Teams</h2>
-                          <ul className="teams__list">
-                            {teamList.map(t => (
-                              <li className="list__item" key={t}>
-                                <Link to="/test">{t}</Link>
-                              </li>
-                            ))}
-                          </ul>
-                        </section>
-                        <section className="info__activity">
-                          <h2 className="info__title">Activity</h2>
-                          <ul className="activity__list">
-                            {logList.map(l => (
-                              <li className="list__item" key={l.activity}>
-                                <div className="box-item">
-                                  <span className="box-image">
-                                    <img src={avatar_url} alt="프로필" />
-                                  </span>
-                                  <p className="box-text">
-                                    <span className="text__activity">
-                                      <strong className="name">
-                                        {prevName !== undefined && prevName !== ''
-                                          ? prevName
-                                          : email.substring(0, email.indexOf('@'))
-                                        }
-                                      </strong>
-                                      <span className="activity">{l.activity}</span>
-                                    </span>
-                                    <span className="text__date">{l.date}</span>
-                                  </p>
-                                </div>
-                              </li>
-                            ))}
-                          </ul>
-                        </section>
-                      </>
-                    )
-                  }
-                </div>
+                {isProfileTab
+                  ? (
+                    <div className="account__info">
+                      <section className="info__profile">
+                        <h2 className="info__title--hidden">Account</h2>
+                        <form className="profile__image" encType="multipart/form-data">
+                          <span className="box-image">
+                            <img src={avatar_url} alt="프로필" />
+                          </span>
+                          <span className="box-input">
+                            <label htmlFor="profile">
+                              <input type="file" name="profile" onChange={e => handleFileInput(e)} />
+                            </label>
+                          </span>
+                        </form>
+                        <span className="profile__desc">
+                          <strong className="desc__name">
+                            {prevName !== undefined && prevName !== ''
+                              ? prevName
+                              : email.substring(0, email.indexOf('@'))
+                            }
+                          </strong>
+                          <button type="button" className="btn-edit" onClick={() => handleEdit()}>Edit profile</button>
+                          <span className="desc__mail">{email}</span>
+                        </span>
+                      </section>
+                      {onEdit
+                        ? (
+                          <form className="form-account" onSubmit={handleSubmit(values => onSubmit(values))}>
+                            <section className="form__section">
+                              <p className="form__data-wrapper">
+                                <span className="wrapper__title">
+                                  <strong className="title">이름*</strong>
+                                </span>
+                                <Field
+                                  name="name"
+                                  type="text"
+                                  label="name"
+                                  placeholder="텍스트 입력"
+                                  component={FormInput}
+                                  validate={[nameRequired, nameLength]}
+                                />
+                              </p>
+                              <p className="form__data-wrapper">
+                                <span className="wrapper__title">
+                                  <strong className="title">연락처*</strong>
+                                </span>
+                                <Field
+                                  name="phone"
+                                  type="tel"
+                                  label="phone"
+                                  placeholder="‘-’ 제외하고 입력"
+                                  component={FormInput}
+                                  validate={[phoneNumRequired, phoneNumRegexp]}
+                                />
+                              </p>
+                            </section>
+                            <section className="form__section">
+                              <p className="form__data-wrapper">
+                                <span className="wrapper__title">
+                                  <strong className="title">비밀번호</strong>
+                                </span>
+                                <button type="button" className="btn-changePw" onClick={e => handlePwPopup(e)}>비밀번호 수정</button>
+                              </p>
+                            </section>
+                            <div className="form__btn-wrapper">
+                              <button type="button" className="btn-cancle" onClick={() => handleEdit()}>취소</button>
+                              <button type="submit" className={`btn-submit${name !== undefined && name !== '' && phone !== undefined && phone !== '' ? '--active' : ''}`}>확인</button>
+                            </div>
+                          </form>
+                        )
+                        : (
+                          <>
+                            <section className="info__teams">
+                              <h2 className="info__title">Teams</h2>
+                              <ul className="teams__list">
+                                {teamList.map(t => (
+                                  <li className="list__item" key={t}>
+                                    <Link to="/test">{t}</Link>
+                                  </li>
+                                ))}
+                              </ul>
+                            </section>
+                            <section className="info__activity">
+                              <h2 className="info__title">Activity</h2>
+                              <ul className="activity__list">
+                                {logList.map(l => (
+                                  <li className="list__item" key={l.activity}>
+                                    <div className="box-item">
+                                      <span className="box-image">
+                                        <img src={avatar_url} alt="프로필" />
+                                      </span>
+                                      <p className="box-text">
+                                        <span className="text__activity">
+                                          <strong className="name">
+                                            {prevName !== undefined && prevName !== ''
+                                              ? prevName
+                                              : email.substring(0, email.indexOf('@'))
+                                            }
+                                          </strong>
+                                          <span className="activity">{l.activity}</span>
+                                        </span>
+                                        <span className="text__date">{l.date}</span>
+                                      </p>
+                                    </div>
+                                  </li>
+                                ))}
+                              </ul>
+                            </section>
+                          </>
+                        )
+                      }
+                    </div>
+                  )
+                  : (
+                    <div className="payment__info">
+                      <div className="wrapper-inner">
+                        <ul className="info__list">
+                          <li className="list__item">
+                            <div className="box-payment">
+                              <span className="payment__title">단일구매</span>
+                              <strong className="payment__plan">PLAN 01</strong>
+                              <span className="payment__step">테스트 진행 중</span>
+                            </div>
+                            <div className="box-paid">
+                              <strong className="paid__total">
+                                222500
+                                <i>원</i>
+                              </strong>
+                              <span className="paid__date">
+                                <span>구매일자: </span>
+                                2019. 7. 23.
+                              </span>
+                            </div>
+                          </li>
+                          <li className="list__item">
+                            <div className="box-payment">
+                              <span className="payment__title">단일구매</span>
+                              <strong className="payment__plan">PLAN 01</strong>
+                              <span className="payment__step">테스트 진행 중</span>
+                            </div>
+                            <div className="box-paid">
+                              <strong className="paid__total">
+                                222500
+                                <i>원</i>
+                              </strong>
+                              <span className="paid__date">
+                                <span>구매일자: </span>
+                                2019. 7. 23.
+                              </span>
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  )
+                }
               </div>
             </section>
             <NewPasswordPopup show={isOpen} onPopup={togglePopup} handleSubmit={handleSubmit} />

3b66b402e914adf930a741688e1dc82fb008d2c3
Injeong Kim <ally.duckling@gmail.com>
2019-10-18 23:22:18 +0900

    report

--- src/containers/NewTestForm/TesrForm/TestFormReport.js
+++ src/containers/NewTestForm/TesrForm/TestFormReport.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { PDFReader } from 'react-read-pdf';
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const TestFormReport = (props) => {
+  const [pageNum, setPageNum] = useState(2);
+
   return (
     <div className="field-wrapper--report">
-      <PDFReader
-        url="https://dbdlab-storage.s3.amazonaws.com/report/2019/10/16/HowToDesignSNGcompressed_4365f.pdf"
-      />
+      <Document
+        file="https://dbdlab-storage.s3.amazonaws.com/report/2019/10/16/HowToDesignSNGcompressed_4365f.pdf"
+        onLoadSuccess={() => console.log('success')}
+        onLoadError={(error) => console.log(error.message)}
+        options={{
+          cMapUrl: `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
+          cMapPacked: true,
+        }}
+      >
+        <Page />
+      </Document>
     </div>
   );
 };

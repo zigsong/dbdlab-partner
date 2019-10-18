367bd9b740bc54820c6875b99ee4eb40b66b1ac3
Injeong Kim <ally.duckling@gmail.com>
2019-10-18 21:37:39 +0900

    report error defining

--- src/containers/NewTestForm/TesrForm/TestFormReport.js
+++ src/containers/NewTestForm/TesrForm/TestFormReport.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { pdfjs, Document, Page } from 'react-pdf';
+import pdf from 'https://dbdlab-storage.s3.amazonaws.com/report/2019/10/16/HowToDesignSNGcompressed_4365f.pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+console.log(pdf);
 
 const TestFormReport = (props) => {
   return (
     <div className="field-wrapper--report">
-      <Document file="https://dbdlab-storage.s3.amazonaws.com/report/2019/10/16/HowToDesignSNGcompressed_4365f.pdf" onLoadError={(error) => console.log('Error while loading page! ' + error.message)}>
+      <Document file={pdf} onLoadError={(error) => console.log('Error while loading page! ' + error.message)}>
         <Page />
       </Document>
     </div>

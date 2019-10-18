bceb67cd01260fa267e9ceda82fc44ee68a5b69b
Injeong Kim <ally.duckling@gmail.com>
2019-10-18 21:32:25 +0900

    error define code added

--- src/containers/NewTestForm/TesrForm/TestFormReport.js
+++ src/containers/NewTestForm/TesrForm/TestFormReport.js
@@ -6,7 +6,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 const TestFormReport = (props) => {
   return (
     <div className="field-wrapper--report">
-      <Document file="https://dbdlab-storage.s3.amazonaws.com/report/2019/10/16/HowToDesignSNGcompressed_4365f.pdf">
+      <Document file="https://dbdlab-storage.s3.amazonaws.com/report/2019/10/16/HowToDesignSNGcompressed_4365f.pdf" onLoadError={(error) => console.log('Error while loading page! ' + error.message)}>
         <Page />
       </Document>
     </div>

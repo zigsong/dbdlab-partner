3a77431b90026c1dfe7dfa2379dbbfc7ccf72720
Injeong Kim <ally.duckling@gmail.com>
2019-10-18 22:02:47 +0900

    Report Test

--- src/containers/NewTestForm/TesrForm/TestFormReport.js
+++ src/containers/NewTestForm/TesrForm/TestFormReport.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { pdfjs, Document, Page } from 'react-pdf';
-import pdf from 'https://dbdlab-storage.s3.amazonaws.com/report/2019/10/16/HowToDesignSNGcompressed_4365f.pdf';
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-console.log(pdf);
+import { PDFReader } from 'react-read-pdf';
 
 const TestFormReport = (props) => {
   return (
     <div className="field-wrapper--report">
-      <Document file={pdf} onLoadError={(error) => console.log('Error while loading page! ' + error.message)}>
-        <Page />
-      </Document>
+      <PDFReader
+        url="https://dbdlab-storage.s3.amazonaws.com/report/2019/10/16/HowToDesignSNGcompressed_4365f.pdf"
+      />
     </div>
   );
 };

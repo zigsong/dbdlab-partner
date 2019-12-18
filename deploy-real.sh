20a68c88164576232c6755578bb0bad63fefd4b6
kyeongyong.c.dbd <kyeongyong.c.dbd@gmail.com>
2019-12-18 20:48:06 +0900

    타입폼 스크립 삽입수정

--- deploy-real.sh
+++ deploy-real.sh
@@ -2,5 +2,5 @@
 
 echo "Deploy To S3 Bucket dbd-realdopt-partner"
 npm run build-real
-aws s3 sync ./build/ s3://dbd-realdopt-partner --profile=jinyoung
-aws cloudfront create-invalidation --distribution-id E2MZ7920GG1TUI --paths "/*" --profile=jinyoung
+aws s3 sync ./build/ s3://dbd-realdopt-partner
+aws cloudfront create-invalidation --distribution-id E2MZ7920GG1TUI --paths "/*" 

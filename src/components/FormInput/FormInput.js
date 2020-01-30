77d85e2a54f9153091021bb9aa7eb82d7d6a7045
PotatoBerry <wlsdud40@gmail.com>
2020-01-31 04:18:39 +0900

    바우처 갯수 숫자만 입력 가능하게

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -13,24 +13,34 @@ const FormInput = ({
   },
   setRef,
   readOnly,
-}) => (
-  <span className={
+}) => {
+  const { onChange } = input;
+
+  return (
+    <span className={
     `box-input__text${!disabled && touched && error ? '--error' : ''}${active ? '--active' : ''}`
   }
-  >
-    <label htmlFor={label}>
-      <input
-        {...input}
-        type={type}
-        placeholder={placeholder}
-        ref={setRef}
-        disabled={disabled}
-        value={defaultValue !== undefined ? defaultValue : input.value}
-        readOnly={readOnly !== undefined ? readOnly : false}
-      />
-    </label>
-    {!disabled && touched && ((error && <span className="msg--error">{error}</span>) || (warning && <span className="msg--warning">{warning}</span>))}
-  </span>
-);
+    >
+      <label htmlFor={label}>
+        <input
+          {...input}
+          type={type === 'number' ? 'text' : type}
+          placeholder={placeholder}
+          ref={setRef}
+          disabled={disabled}
+          onChange={(event) => {
+            if (type === 'number') {
+              event.target.value = event.target.value.replace(/[^0-9]/g, '');
+            }
+            onChange(event);
+          }}
+          value={defaultValue !== undefined ? defaultValue : input.value}
+          readOnly={readOnly !== undefined ? readOnly : false}
+        />
+      </label>
+      {!disabled && touched && ((error && <span className="msg--error">{error}</span>) || (warning && <span className="msg--warning">{warning}</span>))}
+    </span>
+  );
+};
 
 export default FormInput;
